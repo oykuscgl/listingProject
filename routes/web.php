b69a27667cab3b481368f22fb1b17bf9b31cd682
oykuscgl <oyku_sucuoglu@hotmail.com>
2024-07-11 09:19:48 +0300

    backend

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,6 @@ Route::get('/', function () {
 Route::get('/welcome', function () {
     return view('welcome');
 });
-<<<<<<< HEAD
 Route::get('/services', function () {
     return view('services');
 });
@@ -24,40 +23,3 @@ Route::get('/aboutus', function () {
 Route::get('/recipes', function () {
     return view('recipes');
 });
-=======
-
-Route::get('/userHomePage', function () {
-    return view('layouts/userHomePage');
-})->name('userHomePage');
-
-Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
-Route::post('/register', [RegisterController::class, 'register']);
-
-Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [LoginController::class, 'login']);
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-
-Route::get('/hakkımızda', function () {
-    return view('layouts/aboutus');
-});
-
-Route::get('/hizmetlerimiz', function () {
-    return view('layouts/services');
-});
-
-Route::get('/tuketici-arastirmalari', function () {
-    return view('layouts/customersearches');
-});
-
-Route::get('/urun-ekle', function () {
-    return view('admin/products');
-});
-
-Route::middleware(['auth', 'admin'])->group(function () {
-    // Admin Dashboard
-    Route::get('admin/adminDashboard', [AdminController::class, 'dashboard'])->name('adminDashboard');
-});
-
-
-
->>>>>>> origin/main
