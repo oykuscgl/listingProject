609760a405b33d050e73692d2e8f93203f5efa4c
Shayros08 <sercanfteal@gmail.com>
2024-07-22 09:26:38 +0300

    lasts

--- database/migrations/2024_07_19_095134_change_column_type_in_products.php
+++ database/migrations/2024_07_19_095134_change_column_type_in_products.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -11,10 +12,16 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // Geçici olarak sql_mode'u değiştirin
+        DB::statement('SET SESSION sql_mode="NO_ENGINE_SUBSTITUTION";');
+
         Schema::table('products', function (Blueprint $table) {
             // Change column type
             $table->longText('detailed_description')->change();
         });
+
+        // Orijinal sql_mode'u geri yükleyin
+        DB::statement('SET SESSION sql_mode="STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION";');
     }
 
     /**
@@ -23,7 +30,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            //
+            // Orijinal sütun türüne geri dönün
+            $table->text('detailed_description')->change();
         });
     }
 };
